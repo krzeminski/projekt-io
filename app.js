4342d042b6cf2ddd4a6f27a5a961f3ee65aa2a70
krzeminski <49924680+krzeminski@users.noreply.github.com>
2019-11-20 18:47:15 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+//jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
