08798986d92030ab6c5b82901171d36223f1928a
krzeminski <49924680+krzeminski@users.noreply.github.com>
2019-12-06 01:56:51 +0100

    Dodanie wyszukania listy przedmiotow

--- app.js
+++ app.js
@@ -6,32 +6,35 @@ const engine = require(__dirname + "/engine.js");    //module
 
 const app = express();
 
-let state = "all";
-const searchedItemsList = [];
-
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(bodyParser.json());
 
+let state = "all";
+const searchedItemsList = [];
+var accessToken;
+engine.getAccessToken().then(function(res){accessToken=res;});
+
+var exampleLinks = ['https://api.allegro.pl/offers/listing?phrase=Samochód',
+  'https://api.allegro.pl/offers/listing?phrase=Krzeslo&price.from=22&price.to=555&sellingMode.format=BUY_NOW&searchMode=REGULAR&sort=+withDeliveryPrice&limit=50',
+  'https://api.allegro.pl/offers/listing?phrase=Czerwona+sukienka&price.from=22&price.to=555&sort=+withDeliveryPrice&sellingMode.format=BUY_NOW&limit=50&searchMode=REGULAR',
+  'https://api.allegro.pl/offers/listing?phrase=Krzesło&price.from=22&price.to=555&sellingMode.format=BUY_NOW&searchMode=REGULAR&sort=+withDeliveryPrice&limit=50',
+  'https://api.allegro.pl/offers/listing?phrase=Aparat&price.from=1&price.to=56&sellingMode.format=BUY_NOW&searchMode=REGULAR&sort=+withDeliveryPrice&limit=50',
+  'https://api.allegro.pl/offers/listing?phrase=Laptop&price.from=1&price.to=66&sellingMode.format=BUY_NOW&searchMode=REGULAR&sort=+withDeliveryPrice&limit=50',
+  'https://api.allegro.pl/offers/listing?phrase=Kamera&price.from=13&price.to=56&sellingMode.format=BUY_NOW&searchMode=REGULAR&sort=+withDeliveryPrice&limit=50' ];
+
 app.get("/", function(req,res){
   res.render("search",{searchedItemsList:searchedItemsList});
-  // access_token = engine.getAccessToken().then(data => console.log(data));
-  var accessToken = engine.getAccessToken().then(res => {
-    return res;
-  })
-  .catch(err => {
-    console.log(err);
-  });
+  // console.log(accessToken);
+  engine.getOffersListing(exampleLinks[1], accessToken);
 
-  console.log(accessToken);
 
 });
 
 app.post("/", function(req,res){
   const searchedProduct = {
     productName:req.body.productName,
-    quantity:req.body.quantity,
     minPrice:req.body.minPrice,
     maxPrice:req.body.maxPrice,
     state:req.body.state
@@ -43,10 +46,13 @@ app.post("/", function(req,res){
 });
 
 app.get("/result", function(req,res){
+  console.log(searchedItemsList);
+  console.log(engine.getLinks(searchedItemsList));
+  console.log(engine.getL());
   res.sendFile(__dirname + "/views/results.html");
   // res.render("result", {newListItems:workItems});
 });
 
 app.listen(3000, function(){
-  console.log("Server started on port 3000.");
+  console.log("Server started on port 3000...........................................................................................................................");
 });
--- engine.js
+++ engine.js
@@ -2,25 +2,16 @@
 const request = require('request');
 var superagent = require('superagent');
 
-const gettingProducts = [];
-let phraseStart = "https://api.allegro.pl/sale/products?phrase=";
-let phraseEnd =  "&searchMode=REGULAR&sort=price&limit=3";
-
-exports.getSomeProducts = function(searchedProductsList){
-  for(let i = 0; i < searchedProductsList.length; i++){
-    gettingProducts.push(
-      phraseStart + searchedProductsList[i].productName + phraseEnd
-    );
-  }
-  return gettingProducts;
-}
-
+const links = [];
+let phraseStart = "https://api.allegro.pl/offers/listing?phrase=";
+// let phraseEnd =  "&sellingMode.format=BUY_NOW&searchMode=REGULAR&sort=+withDeliveryPrice&limit=50";
+let phraseEnd =  "&sellingMode.format=BUY_NOW";
 
 //Otrzymujemy access_token do autoryzacji
 exports.getAccessToken = async function(){
 	var authUrl = "https://allegro.pl/auth/oauth/token?grant_type=client_credentials";
-  var clientId = "9ba1e6fc865c46379cb6afc1fa548b13";
-  var clientSecret = "2LRgv5M3FISr783Io0n9FbuQdk57GS19HGQV4TJMpbfahsE7OjwOpnvy1lG5W0V7";
+  // var clientId = "9ba1e6fc865c46379cb6afc1fa548b13";
+  // var clientSecret = "2LRgv5M3FISr783Io0n9FbuQdk57GS19HGQV4TJMpbfahsE7OjwOpnvy1lG5W0V7";
   // "clientId:clientSecret" zakodowane w formacie base64:
   var clientAuth = "Basic OWJhMWU2ZmM4NjVjNDYzNzljYjZhZmMxZmE1NDhiMTM6MkxSZ3Y1TTNGSVNyNzgzSW8wbjlGYnVRZGs1N0dTMTlIR1FWNFRKTXBiZmFoc0U3T2p3T3BudnkxbEc1VzBWNw==";
   var accessToken;
@@ -34,7 +25,7 @@ exports.getAccessToken = async function(){
   };
 
   function getToken(){
-     return new Promise(resolve => {
+    return new Promise(resolve => {
       request(options, function(error, response, body){
         if(error){
           console.log(error);
@@ -55,4 +46,62 @@ exports.getAccessToken = async function(){
 
   return accessToken;
 
+}
+
+
+
+
+exports.getLinks = function(searchedProductsList){
+  for(let i = 0; i < searchedProductsList.length; i++){
+    if(searchedProductsList[i].minPrice>searchedProductsList[i].maxPrice){
+      searchedProductsList[i].maxPrice=searchedProductsList[i].minPrice;
+    }
+    links.push(
+      phraseStart + searchedProductsList[i].productName.trim().split(' ').join('+') +
+      "&price.from=" + searchedProductsList[i].minPrice +
+      "&price.to=" + searchedProductsList[i].maxPrice +
+      phraseEnd
+      );
+  }
+  return links;
+}
+exports.getL = function(){return links;}
+
+
+exports.getOffersListing = function(links, token){
+  var lista;
+  var options = {
+    url: links,
+    method: "GET",
+    headers: {
+      "Accept": "application/vnd.allegro.public.v1+json",
+      "content-type": "application/vnd.allegro.public.v1+json",
+      "Authorization": "Bearer " + token
+    },
+    // sort:"+withDeliveryPrice",
+    // limit: "20",
+    // sellingMode:{
+    //   format:"BUY_NOW"
+    // }
+  };
+
+  function getOneOfferListing(){
+    request(options, function(error, response, body){
+      if(error){
+        console.log("Error " + error);
+      }else{
+        if(response.statusCode === 200){
+          lista = JSON.parse(response.body);
+          console.log("Lista: " + lista);
+          return lista;
+        }else{
+          console.log(response);
+        }
+      }
+    });
+  }
+
+  lista = getOneOfferListing();
+
+  return lista;
 }
