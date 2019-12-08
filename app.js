392e3184f24c2aa61bca19f5eeccb6e98d95781e
krzeminski <49924680+krzeminski@users.noreply.github.com>
2019-12-08 12:24:54 +0100

    Wyświetlanie wyników

--- app.js
+++ app.js
@@ -25,6 +25,29 @@ const inputProductsList = [{
 var linksList = [];
 const allProductsList = [];
 var accessToken;
+var shoppingCart = [
+ {
+    sum: 1900,
+    deliveryPrice: 20,
+    product:[{name:"Samsung Galaxy S10 Master Race", price: 1600, seller:"Samsung", link:""},
+            {name:"Usłyszysz nawet ciszę", price: 200, seller:"Samsung", link:""},
+            {name:"Nie zepsuj kabla!", price: 80, seller:"Samsung", link:""}]
+  },
+  {
+    sum: 1950,
+    deliveryPrice: 50,
+    product:[{name:"Na co Ci iphone? Huaweii ftw", price: 1600, seller:"Huaweii", link:""},
+            {name:"takich słuchawek jeszcze nie widziałeś!", price: 200, seller:"Huaweii", link:""},
+            {name:"Podładuj do 100% z nową ładowarką", price: 100, seller:"Huaweii", link:""}]
+  },
+  {
+    sum: 2000,
+    deliveryPrice: 100,
+    product:[{name:"Najlepszy Iphone na rynku !!1!!111!", price: 1200, seller:"apple", link:""},
+            {name:"Słuchawki do niego", price: 400, seller:"apple", link:""},
+            {name:"Ładowarka", price: 300, seller:"apple", link:""}]
+  }
+];
 
 //miejsce then opisane w 12 linijce engine.js
 engine.getAccessToken().then(function(res){accessToken=res;});
@@ -66,8 +89,8 @@ app.get("/result", async function(req,res){
   // console.log(typeof(allProductsList));
   console.log(engine.getDuplicatedSeller(allProductsList));
 
-  res.sendFile(__dirname + "/views/results.html");
-  // res.render("result", {newListItems:workItems});
+  // res.sendFile(__dirname + "/views/results.html");
+  res.render("result", {inputItemList: inputProductsList, cart:shoppingCart});
 });
 
 app.listen(3000, function(){
--- views/index.js
+++
@@ -1,9 +0,0 @@
-$('input[name="maxPrice"]').on('keydown keyup', function(e){
-    if ($(this).val() < $('input[name="minPrice"]')
-        && e.keyCode !==  // keycode for tab
-        && e.keyCode !==  // keycode for enter
-       ) {
-       e.preventDefault();
-       $(this).val($('input[name="minPrice"]'));
-    }
-});
